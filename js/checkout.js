6a075d82079571857df2d3c7cc115a6df2681a23
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-03-03 23:58:50 +0600

    Regenerate HTML when updating delivery option

--- js/checkout.js
+++ js/checkout.js
@@ -5,9 +5,7 @@ import dayjs  from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
 import { deliveryOptions } from "../data/deliveryOptions.js"
 
 
-
-
-
+function renderOrderSummary() {
 
 let cartSummaryHTML = '';
 
@@ -137,5 +135,9 @@ document.querySelectorAll('.js-delivery-option').forEach((element) => {
   element.addEventListener('click', () => {
     const {productId, deliveryOptionId} = element.dataset;
     updateDeliveryOption(productId, deliveryOptionId);
+    renderOrderSummary();
   })
 })
+
+};
+renderOrderSummary();
